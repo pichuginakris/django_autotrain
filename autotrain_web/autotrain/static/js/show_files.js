5c53cd9fe60ebe8bf92abd60d0c2ada342245a2d
Kristina Pichugina <pichuginakris@mail.ru>
2023-06-16 13:42:27 +0300

    add: пояснения к поля настроек

--- autotrain_web/autotrain/static/js/show_files.js
+++ autotrain_web/autotrain/static/js/show_files.js
@@ -1,28 +1,39 @@
-    var photoContainer = document.getElementById('photo-container');
-    var photoItems = Array.from(photoContainer.getElementsByClassName('photo-item'));
-    var currentIndex = 0;
+let inputFile = document.querySelector('.input-file input[type=file]');
 
-    function showPhoto(index) {
-      photoItems.forEach(function(item, i) {
-        if (i === index) {
-          item.style.display = 'block';
-        } else {
-          item.style.display = 'none';
-        }
-      });
-    }
+inputFile.addEventListener('change', function() {
+  let file = this.files[0];
+  let inputParent = this.closest('.input-file');
+  let inputText = inputParent.querySelector('.input-file-text');
+  if (file) {
+    inputText.textContent = file.name;
+    const errorMessage = document.getElementById('training-button');
+      errorMessage.style.background = '#007521';
+      errorMessage.textContent = "Training";
+  }
+});
 
-    function nextPhoto() {
-      currentIndex = (currentIndex + 1) % photoItems.length;
-      showPhoto(currentIndex);
-    }
+function validateForm() {
+  const fileInput = document.querySelector('input[type="file"]');
+  if (fileInput.value === '') {
+    const errorMessage = document.getElementById('training-button');
+    errorMessage.style.background = 'red';
+    errorMessage.textContent = "Please choose a file";
+    return false; // Prevent form submission
+  }
+}
 
-    function prevPhoto() {
-      currentIndex = (currentIndex - 1 + photoItems.length) % photoItems.length;
-      showPhoto(currentIndex);
-    }
+let iconsId = ['gpu-icon', 'dataset-path-icon', 'classes-path-icon', 'speed-icon', 'accuracy-icon'];
+let popupId = ['gpu-popup', 'dataset-path-popup', 'classes-path-popup', 'speed-popup', 'accuracy-popup'];
 
-    document.getElementById('prev-button').addEventListener('click', prevPhoto);
-    document.getElementById('next-button').addEventListener('click', nextPhoto);
+iconsId.forEach(function(iconId, index) {
+  const icon = document.getElementById(iconId);
+  const popup = document.getElementById(popupId[index]);
 
-    showPhoto(currentIndex);
+  document.addEventListener('click', function(event) {
+      if (event.target.matches('#' + iconId)) {
+        popup.style.display = 'block';
+      } else {
+        popup.style.display = 'none';
+      }
+    });
+});
